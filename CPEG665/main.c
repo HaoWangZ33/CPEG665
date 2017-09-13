9e8b7828ba3f0f2deb63b818d5977041aa4c1771
王豪 <fhisti049@gmail.com>
2017-09-13 00:38:07 -0400

    1

--- CPEG665/main.c
+++ CPEG665/main.c
@@ -7,9 +7,44 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    clock_t start, end;
+    int i;
+    int * data = malloc(sizeof(int) * 8000000);
+    
+    printf("Now trying method 1:\n");
+    start = clock();
+    for (i = 1; i <= 4000000; i++){
+        data[2*i - 1] = data[2*i - 1] * 2;
+        data[2*i - 2] = data[2*i - 2] * 3;
+    }
+    end = clock();
+    printf("Finished! It took %lu CPU cycles.\n", (end - start));
+    
+    printf("\nNow trying method 2:\n");
+    start = clock();
+    for (i = 1; i <= 4000000; i++){
+        data[2*i - 2] = data[2*i - 2] * 3;
+        data[2*i - 1] = data[2*i - 1] * 2;
+    }
+    end = clock();
+    printf("Finished! It took %lu CPU cycles.\n", (end - start));
+    
+    printf("\nNow trying method 3:\n");
+    start = clock();
+    for (i = 1; i <= 1000000; i++){
+        for (int y = 0; y < 4; y++) {
+            data[2*i - 1 + y*1000000] = data[2*i - 1 + y*1000000] * 2;
+            data[2*i - 2 + y*1000000] = data[2*i - 2 + y*1000000] * 3;
+            
+        }
+    }
+    end = clock();
+    printf("Finished! It took %lu CPU cycles.\n", (end - start));
+    
+    free(data);
     return 0;
 }
