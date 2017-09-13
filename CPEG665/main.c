d7a52e5214208372e95fa8c650ac02a16cebe3a1
王豪 <fhisti049@gmail.com>
2017-09-13 00:35:19 -0400

    Initial Commit

---
+++ CPEG665/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CPEG665
+//
+//  Created by Hao Wang on 9/13/17.
+//  Copyright © 2017 Hao Wang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
